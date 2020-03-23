b818a2429719bd7dc3e35b8241a6d0b6bcf0daf5
mjalal01 <45449224+mjalal01@users.noreply.github.com>
2020-03-24 00:24:26 +0400

    Corrections

--- src/main/java/com/mehdiyevcs/dao/BookDao.java
+++ src/main/java/com/mehdiyevcs/dao/BookDao.java
@@ -4,5 +4,6 @@ package com.mehdiyevcs.dao;
 import com.mehdiyevcs.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface BookDao extends JpaRepository<Book,Integer> {
 }
